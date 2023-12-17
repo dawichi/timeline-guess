23b535334bce2af0d57e2e7c2b84e7bd81d64758
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-12-17 13:03:01 +0100

    ASTRO -> SVELTE

---
+++ src/components/Card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let title: string
+    export let year: string
+    export let image: string
+</script>
+<div class="bg-zinc-700 w-80 p-4 mx-auto rounded shadow">
+    <h2 class="text-xl font-bold">{String(title)}</h2>
+    <p>{String(year)}</p>
+    <img src={`/images/cards/${image}`} class="w-full rounded" alt={title} />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+
+<div class="container mx-auto grid gap-2">
+    hola
+</div>
+
+<pre class="bg-zinc-800">
+    <!-- {JSON.stringify(game.placedCards)} -->
+    <!-- {cardDisplayed.title} -->
+</pre>
