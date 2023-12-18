81a286f5df837dc150fa26500a21b9fd401d1dd0
David M. Fajardo <51042900+dawichi@users.noreply.github.com>
2023-12-18 14:00:43 +0100

    Update Card.svelte

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -4,10 +4,10 @@
     export let card: Card
     export let showYear: boolean = false
 </script>
-<div class="bg-zinc-700 w-64 p-4 mx-auto rounded shadow" draggable="true">
-    <h2 class="text-xl font-bold pb-2">{card?.title}</h2>
+<div class="bg-zinc-700 w-32 p-4 rounded shadow card draggable cursor-move" draggable="true">
+    <h2 class="font-bold pb-2">{card?.title}</h2>
     {#if showYear}
-        <p class="text-center pb-1 font-bold">year: {card?.year}</p>
+        <p class="text-center pb-1">year: {card?.year}</p>
     {/if}
     <img src={`/images/cards/${card?.image}`} class="w-full rounded" alt={card?.title} />
 </div>
